caebba2878ab9a227d3f9230ab14aeadcfbbd048
刘海泉 <liuhaiquan@clxkj.cn>
2026-03-24 14:41:13 +0800

    B-提交

--- rabbitmq-api/src/main/java/com/kavin/rabbitmq/api/ack/Consumer.java
+++ rabbitmq-api/src/main/java/com/kavin/rabbitmq/api/ack/Consumer.java
@@ -12,7 +12,7 @@ import com.rabbitmq.client.ConnectionFactory;
  */
 public class Consumer {
 
-	
+	//增加备注
 	public static void main(String[] args) throws Exception {
 
 		Channel channel = ChannelUtil.getChannel();
